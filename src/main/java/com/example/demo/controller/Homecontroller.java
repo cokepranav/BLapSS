9bcf0ed92e133e92b4b548b795ab3662520dd0e9
cokepranav <v.saiprani123@gmail.com>
2022-10-25 23:05:27 +0530

    homecontroller

--- src/main/java/com/example/demo/controller/Homecontroller.java
+++ src/main/java/com/example/demo/controller/Homecontroller.java
@@ -1,12 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class Hellocontroller {
+public class Homecontroller {
     @GetMapping({"/hi"})
     public void Home(){
         System.out.println("jsdkklasd");
