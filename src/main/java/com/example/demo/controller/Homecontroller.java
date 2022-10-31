700a66397fba7fcd90b43fbe8e9452c96a7d4616
cokepranav <v.saiprani123@gmail.com>
2022-11-01 02:14:26 +0530

    home page-products

--- src/main/java/com/example/demo/controller/Homecontroller.java
+++ src/main/java/com/example/demo/controller/Homecontroller.java
@@ -1,17 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.dao.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Homecontroller {
+    @Autowired
+    private ProductRepository productRepository;
 //    @GetMapping({"/hi"})
 //    public void Home(){
 //        System.out.println("jsdkklasd");
 ////        return "template/home";
 //    }
     @GetMapping({"/","/index","/home"})
-    public String index(){
-        return "index";
+    public String index(Model model){model.addAttribute("products",productRepository.getAll());return "index";
     }
 }
